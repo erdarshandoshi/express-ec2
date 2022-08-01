562016fdcbe9cbab13b4f7abbb1b44a89eacd9b9
unknown <er.darshandoshi@gmail.com>
2022-08-01 16:14:57 +0530

    added code

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,7 +2,7 @@
 
 #give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ec2-user/express-app
-
+sudo chown -R ec2-user:ec2-user /home/ec2-user/express-app/
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/express-app
 
